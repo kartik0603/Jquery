9e80b6632bed1394342e163b1de0a7ea73a96fd7
kartik0603 <kartikhirapara800@gmail.com>
2024-02-14 22:56:31 +0530

    jQuery

---
+++ task1.js
@@ -0,0 +1,27 @@
+$(".btn1").click(() => {
+  $(".krishna1_img").attr(
+    "src",
+    "https://rukminim2.flixcart.com/image/850/1000/xif0q/poster/w/l/h/medium-krishna-poster-1032-durga-graphics-original-imagt5vv6hywucf6.jpeg?q=20&crop=false"
+  );
+});
+
+$(".btn2").click(() => {
+  $(".krishna2_img").attr(
+    "src",
+    "https://w0.peakpx.com/wallpaper/150/121/HD-wallpaper-sri-krishna-pics-baby-krishna-lord-god-bhakti-devtional.jpg"
+  );
+});
+
+$(".btn3").click(() => {
+  $(".krishna1_img").attr(
+    "src",
+    "https://rukminim2.flixcart.com/image/850/1000/xif0q/poster/w/l/h/medium-krishna-poster-1032-durga-graphics-original-imagt5vv6hywucf6.jpeg?q=20&crop=false"
+  );
+});
+
+$(".btn4").click(() => {
+  $(".krishna2_img").attr(
+    "src",
+    "https://w0.peakpx.com/wallpaper/150/121/HD-wallpaper-sri-krishna-pics-baby-krishna-lord-god-bhakti-devtional.jpg"
+  );
+});
